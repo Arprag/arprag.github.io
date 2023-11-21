a6d72a490a59105a50bce3e36e2cfd15f9a4ea23
Arprag <151455502+Arprag@users.noreply.github.com>
2023-11-21 10:42:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Liste des membres de la famille et de leurs cadeaux souhaités avec URLs
 const familles = [
     { 
-        nom: "Sandrine", 
-        cadeaux: [
+        nom: "Famille Sangarin Vincent", 
+        membres: [
+            { nom: "Sandrine", cadeaux: [
             { nom: "Livre : Tous les styles de Louis XIII à l'art déco", url: "https://fr.shopping.rakuten.com/mfp/3021265/tous-les-styles-du-louis-xiii-a-l-art-deco-sylvie-chadenet-livre?pid=1991419" },
             { nom: "Embellisseur de teint", url: "https://www.magnifaik.com/products/embellisseur-de-teint-cest-la-base" },
             { nom: "Shampoing", url: "https://www.lush.com/fr/fr/p/fairly-traded-honey-shampoo/300g" },
@@ -11,46 +11,84 @@ const familles = [
             { nom: "Boîte à bijoux", url: "https://coulobre.com/products/boite-bijoux-en-bois" },
             { nom: "Thé (Earl Grey)", url: "" }, // Aucune URL pour cet élément
             { nom: "Une place de théâtre", url: "" } // Aucune URL pour cet élément
+            ] },
+            { nom: "Nicolas", cadeaux: [] },
+            { nom: "Suzanne", cadeaux: [] },
+            { nom: "Camille", age: 15, cadeaux: [] },
+            { nom: "Roxane", age: 4, cadeaux: [] }
         ]
     },
-        { 
+    { 
+        nom: "Famille Sangarin David", 
+        membres: [
+            { nom: "Maryse", cadeaux: [] },
+            { nom: "Mickael", cadeaux: [] },
+            { nom: "Océane", age: 13, cadeaux: [] },
+            { nom: "Victor", age: 7, cadeaux: [] }
+        ]
+    },
+    { 
+        nom: "Famille David Le Doaré", 
+        membres: [
+            { nom: "Cécile", cadeaux: [] },
+            { nom: "Mathieu", cadeaux: [] },
+            { nom: "Élise", age: 2, cadeaux: [] }
+        ]
+    },
+    { 
+        nom: "Famille Pavlioglou", 
+        membres: [
+            { nom: "Stéphanie", cadeaux: [] },
+            { nom: "Yann", cadeaux: [] }
+        ]
+    },
+    // Les membres déjà déclarés
+    { 
+        nom: "Sandrine", 
+        cadeaux: [
+            // Liste des cadeaux de Sandrine
+        ]
+    },
+    { 
         nom: "Yannou", 
         cadeaux: [
-            { nom: "Velours de Santal", url: "https://www.labogie.com/products/velours-de-santalz" },
-            { nom: "Oranger en fleur", url: "https://www.labogie.com/products/oranger-en-fleur" },
-            { nom: "Housse de coussin Leonard beige doux", url: "https://www.madura.com/products/housse-de-coussin-leonard-beige-doux-40-40" },
-            { nom: "+ Coussin de garnissage Fibre blanc", url: "https://www.madura.com/products/coussin-de-garnissage-fibre-blanc-40-40" }
+            // Liste des cadeaux de Yannou
         ]
     },
-        { 
+    { 
         nom: "Stéphanie", 
         cadeaux: [
-            { nom: "Vacuvin - Champagne saver & verser", url: "" },
-            { nom: "Lampe de lecture", url: "" },
-            { nom: "Bourjois volume glamour effet push up waterproof", url: "" }
+            // Liste des cadeaux de Stéphanie
         ]
     },
     // Ajoutez ici d'autres membres et leurs listes de cadeaux
 ];
-// Yann, Stéphanie, Suzanne + nous 4
+
 function afficherListes() {
     const container = document.getElementById('listeCadeaux');
     container.innerHTML = ''; // Vider le contenu précédent
 
-    familles.forEach(membre => {
-        const membreDiv = document.createElement('div');
-        membreDiv.innerHTML = `<h2>Liste de ${membre.nom}</h2>`;
+    familles.forEach(famille => {
+        const familleDiv = document.createElement('div');
+        familleDiv.innerHTML = `<h2>Liste de la ${famille.nom}</h2>`;
         
-        const listeUl = document.createElement('ul');
-        membre.cadeaux.forEach(cadeau => {
-            const cadeauLi = document.createElement('li');
-            cadeauLi.innerHTML = cadeau.url 
-                ? `<a href="${cadeau.url}" target="_blank">${cadeau.nom}</a>` 
-                : cadeau.nom;
-            listeUl.appendChild(cadeauLi);
+        famille.membres.forEach(membre => {
+            const membreDiv = document.createElement('div');
+            membreDiv.innerHTML = `<h3>${membre.nom}</h3>`;
+            
+            const listeUl = document.createElement('ul');
+            membre.cadeaux.forEach(cadeau => {
+                const cadeauLi = document.createElement('li');
+                cadeauLi.innerHTML = cadeau.url 
+                    ? `<a href="${cadeau.url}" target="_blank">${cadeau.nom}</a>` 
+                    : cadeau.nom;
+                listeUl.appendChild(cadeauLi);
+            });
+
+            membreDiv.appendChild(listeUl);
+            familleDiv.appendChild(membreDiv);
         });
 
-        membreDiv.appendChild(listeUl);
-        container.appendChild(membreDiv);
+        container.appendChild(familleDiv);
     });
 }
